ee9db4d1d643cbfce4b69648f7529e1e7c798368
MISHELLE SOUSA SILVA <u18289@academico.cotuca.unicamp.br>
2020-03-10 15:06:20 -0300

    Projeto feito no UNITY

---
+++ SistemaSolar/Assets/giraGira.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class giraGira : MonoBehaviour {
+
+  // Use this for initialization
+
+  Vector3 rotacao;
+
+  public float anguloX, anguloY, anguloZ;
+	void Start ()
+  {
+    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+    rotacao = new Vector3(
+                    anguloX * Time.deltaTime,
+                    anguloY * Time.deltaTime,
+                    anguloZ * Time.deltaTime
+                    );
+
+    transform.Rotate(rotacao);
+	}
+}
